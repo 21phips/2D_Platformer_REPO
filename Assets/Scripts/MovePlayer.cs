39768bfac4f8eff12983d5e41b6254579f854201
21phips <79919172+21phips@users.noreply.github.com>
2021-04-15 09:13:21 -0400

    PickUp Collectibles

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MovePlayer : MonoBehaviour
 {
+    public TextMeshProUGUI countText;
     public CharacterController2D controller;
     float horizontalMove = 0f;
     public float runSpeed = 40f;
     bool jump = false;
+    private int count;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -25,8 +29,6 @@ public class MovePlayer : MonoBehaviour
             jump = true;
         }
 
-      
-
     }
 
     void FixedUpdate()
@@ -35,4 +37,22 @@ public class MovePlayer : MonoBehaviour
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;    
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString(); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+      
+    }
+
 }
